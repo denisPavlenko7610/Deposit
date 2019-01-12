05f5ac8cd4bf1951fb9fc46f3b846d3bb6494d4f
Denis7610 <denispavlenko7610@gmail/com>
2019-01-12 15:54:36 +0200

    Update 0.1

--- Deposit/Program.cs
+++ Deposit/Program.cs
@@ -10,6 +10,29 @@ namespace Deposit
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter sum of money: ");
+            double cash = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter procent: ");
+            double procent = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter sum of months: ");
+            double months = Convert.ToDouble(Console.ReadLine());
+
+            double result = deposit(cash, procent, months);
+            Console.WriteLine($"Result : {result}");
+        }
+
+        static public double deposit(double cash,double procent, double months)
+        {
+            double result = 0;
+            double sum = cash;
+
+            for (int i = 0; i < months; i++)
+            {
+                result += (cash * (procent / 100)) / 12;
+            }
+            return result + sum;
         }
     }
 }
